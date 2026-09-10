b98380b5e6bc6d694c57c1fe52cc5d341132adbf
Faisal <faisal602194@gmail.com>
2026-09-10 19:23:56 +0530

    Create motor_speed.c

---
+++ C/motor_speed.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    int rpm;
+
+    printf("Enter the speed of                  motor in rpm:");
+    scanf("%d" ,&rpm);
+    printf("Motor rpm=%d",rpm);
+    return 0;
+}
