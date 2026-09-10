ea59ea892742c36c7aaa0ec1e7daa479f4075d9d
Faisal <faisal602194@gmail.com>
2026-09-10 19:25:54 +0530

    Update motor_speed.c

--- C/motor_speed.c
+++ C/motor_speed.c
@@ -4,8 +4,10 @@ int main()
 {
     int rpm;
 
-    printf("Enter the speed of                  motor in rpm:");
+    printf("Enter the speed of motor in rpm:");
     scanf("%d" ,&rpm);
+    
     printf("Motor rpm=%d",rpm);
+    
     return 0;
 }
